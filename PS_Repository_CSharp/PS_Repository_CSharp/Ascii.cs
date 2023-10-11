106e744d5ea7e2824f4f6a070e7ff3bbd4c2beda
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-12 00:44:29 +0900

    1012
    11654번

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/Ascii.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class Ascii
+	{
+		static void Main(String[] args)
+		{
+			string input = Console.ReadLine();
+			int code = Convert.ToChar(input);
+
+			Console.WriteLine(code);
+		}
+	}
+}
+
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/King.cs
@@ -0,0 +1,16 @@
+﻿
+//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class King
+//	{
+//		static void Main(string[] args)
+//		{
+//            int[] ch = new int[6] { 1, 1, 2, 2, 2, 8 };
+
+//            string[] input = Console.ReadLine().Split(' ');
+			
+//		}
+//	}
+//}
+
