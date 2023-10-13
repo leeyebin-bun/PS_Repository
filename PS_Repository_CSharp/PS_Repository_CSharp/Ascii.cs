a33cefdc9c6cb9f69a186702155ac2342b442f3e
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-14 02:02:45 +0900

    1014
    30점

--- PS_Repository_CSharp/PS_Repository_CSharp/Ascii.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/Ascii.cs
@@ -1,15 +1,15 @@
-﻿using System;
-namespace PS_Repository_CSharp
-{
-	public class Ascii
-	{
-		static void Main(String[] args)
-		{
-			string input = Console.ReadLine();
-			int code = Convert.ToChar(input);
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class Ascii
+//	{
+//		static void Main(String[] args)
+//		{
+//			string input = Console.ReadLine();
+//			int code = Convert.ToChar(input);
 
-			Console.WriteLine(code);
-		}
-	}
-}
+//			Console.WriteLine(code);
+//		}
+//	}
+//}
 
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/Strange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class Strange
+	{
+		static void Main(String[] args)
+		{
+			string input = Console.ReadLine();
+			string[] strArr = input.Split(' ');
+
+			var a = long.Parse(strArr[0]);
+			var b = long.Parse(strArr[1]);
+
+			if (a >= 1 && b <= 100000)
+			{
+                Console.WriteLine((a + b) * (a - b));
+            }
+			 
+		}
+	}
+}
+
+// @ 를 써야 되는 줄 알았다 . ..
