898c3895b772c9f5250d0f3e166262f780c32616
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-15 01:09:41 +0900

    1015

--- PS_Repository_CSharp/PS_Repository_CSharp/Strange.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/Strange.cs
@@ -1,23 +1,21 @@
-﻿using System;
-namespace PS_Repository_CSharp
-{
-	public class Strange
-	{
-		static void Main(String[] args)
-		{
-			string input = Console.ReadLine();
-			string[] strArr = input.Split(' ');
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class Strange
+//	{
+//		static void Main(String[] args)
+//		{
+//			string input = Console.ReadLine();
+//			string[] strArr = input.Split(' ');
 
-			var a = long.Parse(strArr[0]);
-			var b = long.Parse(strArr[1]);
+//			var a = long.Parse(strArr[0]);
+//			var b = long.Parse(strArr[1]);
 
-			if (a >= 1 && b <= 100000)
-			{
-                Console.WriteLine((a + b) * (a - b));
-            }
-			 
-		}
-	}
-}
+//          Console.WriteLine((a + b) * (a - b));
+            		 
+//		}
+//	}
+//}
 
-// @ 를 써야 되는 줄 알았다 . ..
+//// 1. @ 를 써야 되는 줄 알았다 . ..
+//// 2. int 가 아니라 long 이었다 
