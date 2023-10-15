88d99b1e9f18f453469e0039aab69a7721542880
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-15 23:33:25 +0900

    1015

--- PS_Repository_CSharp/PS_Repository_CSharp/King.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/King.cs
@@ -1,23 +1,23 @@
 ﻿
-using System;
-namespace PS_Repository_CSharp
-{
-	public class King
-	{
-		static void Main(string[] args)
-		{
-			int[] ch = new int[6] { 1, 1, 2, 2, 2, 8 };
+//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class King
+//	{
+//		static void Main(string[] args)
+//		{
+//			int[] ch = new int[6] { 1, 1, 2, 2, 2, 8 };
 
-			string[] input = Console.ReadLine().Split(' ');
+//			string[] input = Console.ReadLine().Split(' ');
 
-			for(int i = 0; i < input.Length; i++)
-			{
-				ch[i] -= int.Parse(input[i]);
-			}
+//			for(int i = 0; i < input.Length; i++)
+//			{
+//				ch[i] -= int.Parse(input[i]);
+//			}
 
-			Console.WriteLine(string.Join(" ", ch));
-			//string.join 문자열 배열 내의 복수개의 요소들을 이어 붙일 수 있다. 
-		}
-	}
-}
+//			Console.WriteLine(string.Join(" ", ch));
+//			//string.join 문자열 배열 내의 복수개의 요소들을 이어 붙일 수 있다. 
+//		}
+//	}
+//}
 
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/star.cs
@@ -0,0 +1,30 @@
+﻿
+//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class star
+//	{
+//		static void Main(String[] args)
+//		{
+//			string input = Console.ReadLine();
+//			int star = int.Parse(input);
+
+//			for(var i = 1; i <= star; i++)
+//			{
+//				for(var j = 1; j <= star; j++)
+//				{
+                 
+//					if (star - i >= j)
+//						Console.Write(" ");
+//					else
+//					{
+//						Console.Write("*");
+//					}
+					 
+//                }
+//				Console.WriteLine();
+//            }
+//		}
+//	}
+//}
+
