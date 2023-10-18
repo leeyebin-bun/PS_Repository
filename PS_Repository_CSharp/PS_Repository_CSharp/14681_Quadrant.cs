d1f102c4cf82a4a7403d710288676ed950377d3a
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-19 00:07:27 +0900

    1019

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/14681_Quadrant.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _4681_Quadrant
+//	{
+//		static void Main(string[] args)
+//		{
+//			int x = int.Parse(Console.ReadLine());
+//			int y = int.Parse(Console.ReadLine());
+
+//			if(x > 0 && y > 0)
+//			{
+//                Console.WriteLine("1");
+//            }
+//			else if(x > 0 && y < 0)
+//			{
+//                Console.WriteLine("4");
+//            }
+//			else if(x < 0 && y < 0)
+//			{
+//                Console.WriteLine("3");
+//            }
+//			else
+//			{
+//                Console.WriteLine("2");
+//            }
+			
+//		}
+//	}
+//}
+
