1f4bf4d2356e18acfd4aa0fe0a09e1a55e0d1768
leeyebin-bun <52908108+leeyebin-bun@users.noreply.github.com>
2023-10-19 02:17:11 +0900

    1019

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/11945_HotFish.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _1945_HotFish
+//	{
+//		static void Main(String[] args)
+//		{
+//			string input = Console.ReadLine();
+//			string[] strArr = n.Split(' ');
+
+//			var n = int.Parse(strArr[0]);
+//            var m = int.Parse(strArr[1]);
+
+//			Console.WriteLine();
+//        }
+//	}
+//}
+
+
+//뭔말이야 ..
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/5597_Homework.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _597_Homework
+//	{
+//		static void Main(String[] args)
+//		{
+//			int[] homework = new int[31]; 
+
+//			for(var i = 0; i < 28; i++) // 제출한 28명의
+//			{
+//				int n = int.Parse(Console.ReadLine()); // 출석번호를 입력받아서
+//				homework[n] = 1; // 출석번호에 해당하는 배열의 값을 1로 만든다.
+//			}
+
+//            for (var j = 1; j <= 30; j++) // 모든 학생을 조회
+//            {
+//                if (homework[j] != 1) // 출석번호가 1로 바뀌지 않았다면
+//                    Console.WriteLine(j); // 출력 ( 작은값부터 )
+//            }
+            
+//        }
+//	}
+//}
+
