0dde0dda9908b9f187bd1f5daa9a46814277ab76
yebin <na04232@naver.com>
2023-10-29 22:20:04 +0900

    1029

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/10101_triangle.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _0101_triangle
+//	{
+//		static void Main(String[] args)
+//		{
+//			int a = int.Parse(Console.ReadLine());
+//			int b = int.Parse(Console.ReadLine());
+//			int c = int.Parse(Console.ReadLine());
+
+//			int sum = a + b + c;
+//			if (a == 60 && b == 60 && c == 60)
+//			{
+//				Console.WriteLine("Equilateral");
+//			}
+//			else if (sum == 180 && a == b || b == c|| a == c)
+//			{
+//				Console.WriteLine("Isosceles");
+//			}
+//			else if(sum == 180)
+//			{
+//				Console.WriteLine("Scalene");
+//			}
+//			else
+//			{
+//				Console.WriteLine("Error");
+//			}
+
+		
+//        }
+//	}
+//}
+
