e7084a96334e0755e7b6e17519f5609a23eec9eb
yebin <na04232@naver.com>
2023-10-29 21:46:58 +0900

    1029

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/10797_10S.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _0797_10S
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+//			string[] strArr = Console.ReadLine().Split(' ');
+//			int count = 0;
+
+//			int a = int.Parse(strArr[0]);
+//			int b = int.Parse(strArr[1]);
+//			int c = int.Parse(strArr[2]);
+//			int d = int.Parse(strArr[3]);
+//			int e = int.Parse(strArr[4]);
+
+//            foreach (var u in strArr)
+//            {
+//                int number = int.Parse(u);
+//                if (number == input)
+//                {
+//                    count++;
+//                }
+//            }
+
+//            Console.WriteLine(count);
+//		}
+//	}
+//}
+
