ec400f6cfbedeca62ad2f67f7c46aab390b9fcf4
yebin <na04232@naver.com>
2023-10-31 00:00:24 +0900

    1031

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/10817_ThreeCount.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _0817_ThreeCount
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(' ');
+
+
+//			int[] answer = { int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]) };
+
+//   //       //int a = int.Parse(input[0]);
+//			//int b = int.Parse(input[1]);
+//			//int c = int.Parse(input[2]);
+
+//			Array.Sort(answer);
+//			//작은순서대로 배열해놓고 [1]출력
+
+//			Console.WriteLine(answer[1]);
+//		}
+//	}
+//}
+
