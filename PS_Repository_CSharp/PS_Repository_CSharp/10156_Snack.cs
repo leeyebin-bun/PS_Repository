bf4def630c4d03a2fc3ae8fef779c7f51df9bc55
yebin <na04232@naver.com>
2023-10-30 21:26:31 +0900

    1030

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/10156_Snack.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _0156_Snack
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(' ');
+
+//			int answer = 0;
+
+//			int k = int.Parse(input[0]);
+//			int n = int.Parse(input[1]);
+//			int m = int.Parse(input[2]);
+
+//			answer = (k * n) - m;
+
+//			if (answer < 0)
+//				Console.WriteLine("0");
+//			else
+//			Console.WriteLine(answer);
+//		}
+//	}
+//}
+
