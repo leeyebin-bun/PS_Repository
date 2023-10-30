4f0254ac8899ba6a6d3a4ed0b08bf95a101e2c10
yebin <na04232@naver.com>
2023-10-31 01:36:50 +0900

    1031

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/5532_WeekendHomework.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _532_WeekendHomework
+//	{
+//		static void Main(String[] args)
+//		{			
+
+//			int l = int.Parse(Console.ReadLine());
+//            int a = int.Parse(Console.ReadLine());
+//            int b = int.Parse(Console.ReadLine());
+//            int c = int.Parse(Console.ReadLine());
+//            int d = int.Parse(Console.ReadLine());
+
+//            int answer = 0;
+//            int answer_2 = 0;
+
+//            if(a%c == 0)
+//            {
+//                answer = a / c;
+//            }
+//            else
+//            {
+//                answer = a / c + 1;
+//            }
+
+//            if(b%d == 0)
+//            {
+//                answer_2 = b / d;
+//            }
+//            else
+//            {
+//                answer_2 = b / d + 1;
+//            }
+            
+//			Console.WriteLine(Math.Min(l-answer , l-answer_2));
+//        }
+//	}
+//}
+
+////.....^^;...
