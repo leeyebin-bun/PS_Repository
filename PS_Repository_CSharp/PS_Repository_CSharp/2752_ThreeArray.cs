3c7a15c43890d642c36157d94ebf1fb79fbe7eac
yebin <na04232@naver.com>
2023-10-31 00:19:43 +0900

    1031

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2752_ThreeArray.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _752_ThreeArray
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(' ');
+
+//			int[] answer = { int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]) };
+//			Array.Sort(answer);
+
+//			Console.WriteLine(answer[0] + " " + answer[1] + " " + answer[2]);
+          
+//        }
+//	}
+//}
+
