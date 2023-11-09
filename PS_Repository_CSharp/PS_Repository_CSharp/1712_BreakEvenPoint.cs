6bed262dada2433f9c6055c7eb26360300c09e58
yebin <na04232@naver.com>
2023-11-09 23:23:11 +0900

    1109

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/1712_BreakEvenPoint.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _712_BreakEvenPoint
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(" ");
+
+//			int a = int.Parse(input[0]);
+//            int b = int.Parse(input[1]);
+//            int c = int.Parse(input[2]);
+
+//			if(b >= c)
+//			{
+//				Console.WriteLine(-1);
+//			}
+//			else
+//			{
+//				Console.WriteLine(a / (c - b) + 1);
+//			}
+//		}
+//	}
+//}
+
+////문제가 . ..  무슨 . . .. . . . .. .. . . . . .......ㅠㅠ... .  
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/1978_DecimalFind.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _978_DecimalFind
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+
+//			string[] input_2 = Console.ReadLine().Split(' ');
+//			int answer = 0;
+
+//			int a = int.Parse(input_2[0]);
+//            int b = int.Parse(input_2[1]);
+//            int c = int.Parse(input_2[2]);
+//            int d = int.Parse(input_2[3]);
+//			
+//                if (a % 2 != 0 && a != 1)
+//                    answer += 1;
+//                if (b % 2 != 0 && b != 1)
+//                    answer += 1;
+//                if (c % 2 != 0 && c != 1)
+//                    answer += 1;
+//                if (d % 2 != 0 && d != 1)
+//                    answer += 1;             
+			
+//			Console.WriteLine(answer);
+//		}
+//	}
+//}
+
