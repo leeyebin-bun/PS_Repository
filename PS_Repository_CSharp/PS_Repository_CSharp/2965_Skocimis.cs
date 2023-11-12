cb6c490d5b3bdd96afdd2ce6370b647a96b102e8
yebin <na04232@naver.com>
2023-11-12 17:40:45 +0900

    1112

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2965_Skocimis.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _965_Skocimis
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(' ');
+
+//			int a = int.Parse(input[0]);
+//            int b = int.Parse(input[1]);
+//            int c = int.Parse(input[2]);
+
+//			int max_1 = b - a;
+//			int max_2 = c - b;
+
+//			var answer = MathF.Max(max_1, max_2);
+
+//			Console.WriteLine(answer - 1);
+//        }
+//	}
+//}
+
