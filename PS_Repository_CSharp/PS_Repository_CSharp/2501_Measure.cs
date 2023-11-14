1ecae14ca9c0f747fca561c497ff7bacc7ddec50
yebin <na04232@naver.com>
2023-11-14 21:54:32 +0900

    1114

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2501_Measure.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _501_Measure
+//	{
+//		static void Main(String[] args)
+//		{
+//            string[] input = Console.ReadLine().Split();
+
+//            int n = int.Parse(input[0]);
+//            int k = int.Parse(input[1]);
+
+//            List<int> answer = new List<int>();
+
+//            for (var i = 1; i <= n; i++)
+//            {
+//                if (n % i == 0)
+//                {
+//                    answer.Add(i);
+//                }
+
+//                if (answer.Count >= k)
+//                {
+//                    Console.WriteLine(answer[k - 1]); 
+//                    return;
+//                }
+//            }
+
+//            Console.WriteLine(0); 
+//        }
+//	}
+//}
+
