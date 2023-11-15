a43fe0385e8ec1f98af089b14c161a0b0c0de2b0
yebin <na04232@naver.com>
2023-11-15 15:15:59 +0900

    1115

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2490_YPlay.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _490_YPlay
+//	{
+//		static void Main(String[] args)
+//		{
+
+//            for (int i = 0; i < 3; i++)
+//            {
+//                int sum = 0;
+
+//                string[] s = Console.ReadLine().Split(' ');
+
+//                for (int j = 0; j < 4; j++)   
+//                {
+//                    int a = int.Parse(s[j]);
+//                    sum += a;
+//                }
+
+//                if (sum == 3) Console.WriteLine("A");
+//                else if (sum == 2) Console.WriteLine("B");
+//                else if (sum == 1) Console.WriteLine("C");
+//                else if (sum == 0) Console.WriteLine("D");
+//                else if (sum == 4) Console.WriteLine("E");
+//            }
+//        }
+//	}
+//}
+
+//// 후엥 컨닝.. .  . .. . .  ㅋ
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2908_Define.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _908_Define
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split();
+
+//			string a = input[0];
+//			string b = input[1];
+
+//			var reverseA = new string(a.Reverse().ToArray());
+//            var reverseB = new string(b.Reverse().ToArray());
+
+//			var answerA = int.Parse(reverseA);
+//			var answerB = int.Parse(reverseB);
+
+//			if (answerA > answerB)
+//				Console.WriteLine(reverseA);
+//			else
+//				Console.WriteLine(reverseB);
+
+//        }
+//	}
+//}
+
+//// 아... 그 상수가 아니었네
