d82669821a8e65142d5f532411a690cb01ac207f
yebin <na04232@naver.com>
2023-12-02 21:30:21 +0900

    1202

--- PS_Repository_CSharp/PS_Repository_CSharp/5565_Receipt.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/5565_Receipt.cs
@@ -1,21 +1,21 @@
 ﻿//using System;
 //namespace PS_Repository_CSharp
 //{
-//	public class _565_Receipt
-//	{
-//            static void Main(string[] args)
+//    public class _565_Receipt
+//    {
+//        static void Main(string[] args)
+//        {
+//            int input = int.Parse(Console.ReadLine());
+
+//            for (int i = 0; i < 9; i++)
 //            {
-//                int input = int.Parse(Console.ReadLine());
-
-//                for (int i = 0; i < 9; i++)
-//                {
-//                    input -= int.Parse(Console.ReadLine());
-//                }
-
-//                Console.WriteLine(input);
+//                input -= int.Parse(Console.ReadLine());
 //            }
+
+//            Console.WriteLine(input);
 //        }
-//	}
+//    }
+//}
 
 
-// 컨닝 . .. . .  .   
+//컨닝. .. . .  .   
