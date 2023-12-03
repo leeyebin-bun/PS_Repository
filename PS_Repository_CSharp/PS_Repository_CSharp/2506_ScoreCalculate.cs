afd84bdd826f0ca7c0be3ff4e3c33e83783b2aaf
yebin <na04232@naver.com>
2023-12-03 16:02:40 +0900

    1203

--- PS_Repository_CSharp/PS_Repository_CSharp/2506_ScoreCalculate.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/2506_ScoreCalculate.cs
@@ -1,34 +1,34 @@
-﻿using System;
+﻿//using System;
 
-namespace PS_Repository_CSharp
-{
-    public class _506_ScoreCalculate
-    {
-        static void Main(String[] args)
-        {
-            int input = int.Parse(Console.ReadLine());
-            int total = 0;
-            int score = 0;
+//namespace PS_Repository_CSharp
+//{
+//    public class _506_ScoreCalculate
+//    {
+//        static void Main(String[] args)
+//        {
+//            int input = int.Parse(Console.ReadLine());
+//            int total = 0;
+//            int score = 0;
 
-            string[] inputNumbers = Console.ReadLine().Split(' ');
+//            string[] inputNumbers = Console.ReadLine().Split(' ');
 
-            for (int i = 0; i < input; i++)
-            {
-                int b = int.Parse(inputNumbers[i]);
+//            for (int i = 0; i < input; i++)
+//            {
+//                int b = int.Parse(inputNumbers[i]);
 
-                if (b == 1)
-                {
-                    score++;
-                    total += score;
+//                if (b == 1)
+//                {
+//                    score++;
+//                    total += score;
              
-                }
-                else
-                {
-                    score = 0;
-                }
-            }
+//                }
+//                else
+//                {
+//                    score = 0;
+//                }
+//            }
 
-            Console.WriteLine(total);
-        }
-    }
-}
+//            Console.WriteLine(total);
+//        }
+//    }
+//}
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/8958_OX.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _958_OX
+//	{
+//		static void Main(string[] Args)
+//		{
+//			int test = int.Parse(Console.ReadLine());
+
+//			for (var i = 0; i < test; i++)
+//			{
+//				string a = Console.ReadLine();
+
+//                int sum = 0;
+//                int num = 0;
+
+//                foreach (var b in a)
+//				{
+//					if(b == 'O')
+//					{
+//						num++;
+//						sum += num;
+//					}
+//					else
+//					{
+//						num = 0;
+//					}
+					
+//				}
+//				Console.WriteLine(sum);
+//			}
+			
+
+//		}
+//	}
+//}
+
