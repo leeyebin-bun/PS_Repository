4179136cd228566339a72aa23fca34c1fa34b5e2
yebin <na04232@naver.com>
2023-12-06 12:29:14 +0900

    1206

--- PS_Repository_CSharp/PS_Repository_CSharp/10809_Alphabet.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/10809_Alphabet.cs
@@ -1,12 +1,23 @@
-﻿//using System;
-//namespace PS_Repository_CSharp
-//{
-//    public class _0809_Alphabet
-//    {
-//        static void Main(String[] args)
-//        {
-            
-//        }
-//    }
-//}
+﻿using System;
+namespace PS_Repository_CSharp
+{
+    public class _0809_Alphabet
+    {
+        static void Main(String[] args)
+        {
+            string S = Console.ReadLine();
 
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(S.IndexOf((char)(97 + i))); // 97:a , 98:b , 99:c , 100:d .....
+
+                Console.Write(' ');
+            }
+
+        }
+    }
+}
+
+// 소문자 아스키코드 : 97 ~ 122
+// indexOf( 찾을 문자열 ) 
+// indexOf 함수에서 결과값을 찾을 수 없/ 경우엔 -1 을 반환한다.
