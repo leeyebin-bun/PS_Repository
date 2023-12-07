a47d35b743f495455076f26759c34adbcee533b2
yebin <na04232@naver.com>
2023-12-07 16:47:08 +0900

    1207

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2588_Multiplication.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _588_Multiplication
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+//			string aa = Console.ReadLine();
+
+//			Console.WriteLine(input * int.Parse(aa[2].ToString()));
+//            Console.WriteLine(input * int.Parse(aa[1].ToString()));
+//            Console.WriteLine(input * int.Parse(aa[0].ToString()));
+//            Console.WriteLine(input * int.Parse(aa));
+//        }
+//	}
+//}
+
+//// int.parse() : string -> int 변환
+//// string 을 자르면 char 형식으로 변한다
