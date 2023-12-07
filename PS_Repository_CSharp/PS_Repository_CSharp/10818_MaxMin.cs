699abed62eecc160242411ec2abcd8201572b0cc
yebin <na04232@naver.com>
2023-12-07 15:52:51 +0900

    1207

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/10818_MaxMin.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _0818_MaxMin
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+//			string[] count = Console.ReadLine().Split(' ');
+
+//			int[] test = new int[input];
+
+//			for(var i = 0; i < input; i++)
+//			{
+//				test[i] = int.Parse(count[i]);
+//			}
+
+//            Console.WriteLine(test.Min() + " " + test.Max());
+//		}
+//	}
+//}
+
