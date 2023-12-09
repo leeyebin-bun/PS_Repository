8691142f3d92d690684d25de9f99130f5f33f86d
yebin <na04232@naver.com>
2023-12-09 16:11:23 +0900

    1209

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2903_Center.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _903_Center
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+
+//			int a = 3;
+//			int b = a - 1;
+
+//			int sum = 0;
+
+//			for(int i = 0; i < input; i++)
+//			{
+//				sum = a * a;
+//				a = a + b;
+//				b = a - 1;
+//			}
+
+//			Console.WriteLine(sum);
+
+//		}
+//	}
+//}
+
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/3009_Fourth.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _009_Fourth
+//	{
+//		static void Main(String[] args)
+//		{
+
+//			List<int> x = new List<int>();
+//			List<int> y = new List<int>();
+
+//			for (var i = 0; i < 3; i++)
+//			{
+//				string[] input = Console.ReadLine().Split(' ');
+
+//				int a = int.Parse(input[0]);
+//				int b = int.Parse(input[1]);
+
+//				if(x.Contains(a))
+//				{
+//					x.Remove(a);
+//				}
+//				else
+//				{
+//					x.Add(a);
+//				}
+
+//                if (y.Contains(b))
+//                {
+//                    y.Remove(b);
+//                }
+//                else
+//                {
+//                    y.Add(b);
+//                }
+//            }
+
+//			Console.WriteLine(x[0] + " " + y[0]);
+//		}
+//	}
+//}
+
