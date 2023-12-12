3a346bf637a8540fa4f4d0ab5ed7233531698f9e
yebin <na04232@naver.com>
2023-12-12 23:53:52 +0900

    1212

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/17496_StarFruit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class _7496_StarFruit
+	{
+		static void Main(String[] args)
+		{
+			string[] input = Console.ReadLine().Split(' ');
+
+			int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+            int sum = int.Parse(input[3]);
+
+			int output = 0;
+
+			if(b%c == 1)
+			{
+                output = ((a / b) * c) * sum;
+            }
+			else
+			{
+                output = ((a / b - 1) * c) * sum;
+            }
+			
+
+			Console.WriteLine(output);
+
+        }
+	}
+}
+
