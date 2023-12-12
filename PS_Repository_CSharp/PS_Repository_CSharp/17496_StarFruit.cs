309256621e8b58d3c4589b4a89dc9b80b8d91b97
yebin <na04232@naver.com>
2023-12-13 00:57:24 +0900

    1212

--- PS_Repository_CSharp/PS_Repository_CSharp/17496_StarFruit.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/17496_StarFruit.cs
@@ -1,32 +1,21 @@
-﻿using System;
-namespace PS_Repository_CSharp
-{
-	public class _7496_StarFruit
-	{
-		static void Main(String[] args)
-		{
-			string[] input = Console.ReadLine().Split(' ');
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _7496_StarFruit
+//	{
+//		static void Main(String[] args)
+//		{
+//			string[] input = Console.ReadLine().Split(' ');
 
-			int a = int.Parse(input[0]);
-            int b = int.Parse(input[1]);
-            int c = int.Parse(input[2]);
-            int sum = int.Parse(input[3]);
+//			int summerDay = int.Parse(input[0]); 
+//            int growDay = int.Parse(input[1]);
+//            int star = int.Parse(input[2]);
+//            int money = int.Parse(input[3]);
 
-			int output = 0;
 
-			if(b%c == 1)
-			{
-                output = ((a / b) * c) * sum;
-            }
-			else
-			{
-                output = ((a / b - 1) * c) * sum;
-            }
-			
+//		    Console.WriteLine((summerDay - 1 ) / growDay * star * money);
 
-			Console.WriteLine(output);
-
-        }
-	}
-}
+//        }
+//	}
+//}
 
