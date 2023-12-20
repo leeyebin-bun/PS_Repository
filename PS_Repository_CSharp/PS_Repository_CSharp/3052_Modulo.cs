f2dca04f0bab153a8717dfc0bcb73abd1b2c42f9
yebin <na04232@naver.com>
2023-12-20 18:18:31 +0900

    1220

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/3052_Modulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class _052_Modulo
+	{
+		static void Main(String[] args)
+		{
+			
+			for(int i = 0; i < 10; i++)
+			{
+				int input = int.Parse(Console.ReadLine());
+                Console.WriteLine(input % 42);
+            }
+
+			
+		}
+	}
+}
+
--- PS_Repository_CSharp/PS_Repository_CSharp/5073_Triangles.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/5073_Triangles.cs
@@ -1,12 +1,44 @@
-﻿using System;
-namespace PS_Repository_CSharp
-{
-	public class _073_Triangles
-	{
-		static void Main(String[] args)
-		{
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _073_Triangles
+//	{
+//		static void Main(String[] args)
+//		{
+//			for (int i = 0; i < 5; i++)
+//			{
+//				string[] input = Console.ReadLine().Split(' ');
 
-		}
-	}
-}
+//				int a = int.Parse(input[0]);
+//				int b = int.Parse(input[1]);
+//				int c = int.Parse(input[2]);
+
+//				if (a == 0 && b == 0 && c == 0)
+//				{
+//					return;
+//				}
+
+//				if (a >= b + c || b >= a + c || c >= a + b)
+//				{
+//					Console.WriteLine("Invalid");
+//				}
+//				else if (a == b && b == c)
+//				{
+//					Console.WriteLine("Equilateral");
+//				}
+//				else if (a == b || b == c || a == c)
+//				{
+//					Console.WriteLine("Isosceles");
+//				}
+//				else if (a != b && b != c || a != c)
+//				{
+//                    Console.WriteLine("Scalene");
+//                }
+
+				
+				
+//            }
+//		}
+//	}
+//}
 
