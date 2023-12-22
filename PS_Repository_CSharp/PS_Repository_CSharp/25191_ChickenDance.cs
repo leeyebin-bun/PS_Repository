7724ae3a2399f2b7c27f0279755d7405591ecdae
yebin <na04232@naver.com>
2023-12-22 22:58:28 +0900

    1222

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/25191_ChickenDance.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _5191_ChickenDance
+//	{
+//		static void Main(String[] args)
+//		{
+//            int chicken = int.Parse(Console.ReadLine());
+//            int[] cokeBeer = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+//            int answer = (cokeBeer[0] / 2) + cokeBeer[1];
+
+//            Console.WriteLine(chicken < answer ? chicken : answer);
+
+//			/*
+//            int chicken = int.Parse(Console.ReadLine());
+//			string[] cokeBeer = Console.ReadLine().Split(' ');
+//			int coke = int.Parse(cokeBeer[0]);
+//			int beer = int.Parse(cokeBeer[1]);
+//			int answer = 0;
+
+//			while(true)
+//			{
+//                coke -= 2;
+//                answer++;
+
+//				if (coke == 0)
+//					break;
+//            }
+
+//			while(true)
+//			{
+//                beer -= 1;
+//                answer++;
+
+//				if (beer == 0)
+//					break;
+//            }
+
+//            if (chicken < answer)
+//            {
+//				Console.WriteLine(chicken);
+//            }
+//			else
+//			{
+//                Console.WriteLine(answer);
+//            }
+//			*/
+//        }
+//	}
+//}
+
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/25238_MapleStory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class _5238_MapleStory
+	{
+		static void Main(String[] args)
+		{
+			string[] input = Console.ReadLine().Split(' ');
+
+			int a = int.Parse(input[0]);
+			int b = int.Parse(input[1]);
+
+			int damage = 100 * a - a * b;
+
+
+            if (damage > 100)
+				Console.WriteLine(0);
+			else
+				Console.WriteLine(1);
+		}
+	}
+}
+
