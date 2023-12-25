0e377c1d0bc1e47d909c746974fe5c8b82452e5b
yebin <na04232@naver.com>
2023-12-25 21:52:06 +0900

    1225
    메리크리스먀스 . . .

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/24723_GreenTop.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _4723_GreenTop
+//	{
+//		static void Main(String[] args)
+//		{
+//			int input = int.Parse(Console.ReadLine());
+
+//			double answer = Math.Pow(2, input);
+
+//            Console.WriteLine(answer);
+//		}
+//	}
+//}
+
