ba50ed71e6254879baeca596fce9a34e52a655b0
yebin <na04232@naver.com>
2023-12-27 15:32:14 +0900

    1227

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2577_NumberCount.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _577_NumberCount
+//	{
+//		static void Main(String[] args)
+//		{
+//			int a = int.Parse(Console.ReadLine());
+//			int b = int.Parse(Console.ReadLine());
+//			int c = int.Parse(Console.ReadLine());
+			    
+//			int mul = a * b * c;
+
+//			int[] answer = new int[10];
+
+//			while(true)
+//			{
+//				if(mul == 0)
+//				{
+//					break;
+//				}
+
+//				int i = mul % 10; // mul 의 마지막 숫자
+//				answer[i]++;  // i 가 7이라고 한다면 answer 배열의 7번째 카운트 +1
+//                mul = mul / 10; // 17037300 , 1703730 , 170373 , 17037 , 1703 ....
+
+//            }
+
+//			for(int i = 0; i < answer.Length; i++)
+//			{
+//				Console.WriteLine(answer[i]);
+//			}
+
+
+//		}
+//	}
+//}
+
---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2959_Turtle.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//namespace PS_Repository_CSharp
+//{
+//	public class _959_Turtle
+//	{
+//		static void Main(String[] args)
+//		{
+
+//		}
+//	}
+//}
+
