c014dcd473538a6dae91745f3d54722dcef2e479
yebin <na04232@naver.com>
2024-01-02 23:42:06 +0900

    0102
    중간커밋  . .

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2750_NumSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class _750_NumSort
+	{
+		static void Main(String[] args)
+		{
+			int input = int.Parse(Console.ReadLine());
+			List<int> answer = new List<int>();
+
+			for(int i = 0; i < input; i++)
+			{
+                int sortNum = int.Parse(Console.ReadLine());
+				answer.Add(sortNum);
+
+					answer.Sort();
+                
+            }
+            Console.WriteLine(answer);
+
+
+        }
+	}
+}
+
