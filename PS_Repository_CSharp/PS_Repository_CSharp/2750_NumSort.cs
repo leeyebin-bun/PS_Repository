1fefd7e6572b58a5e66bb939d3f1045c94e4b03f
yebin <na04232@naver.com>
2024-01-04 15:40:36 +0900

    0104

--- PS_Repository_CSharp/PS_Repository_CSharp/2750_NumSort.cs
+++ PS_Repository_CSharp/PS_Repository_CSharp/2750_NumSort.cs
@@ -1,25 +1,27 @@
-﻿using System;
-namespace PS_Repository_CSharp
-{
-	public class _750_NumSort
-	{
-		static void Main(String[] args)
-		{
-			int input = int.Parse(Console.ReadLine());
-			List<int> answer = new List<int>();
+﻿//using System;
+//using System.Collections.Generic;
 
-			for(int i = 0; i < input; i++)
-			{
-                int sortNum = int.Parse(Console.ReadLine());
-				answer.Add(sortNum);
+//namespace PS_Repository_CSharp
+//{
+//    public class _750_NumSort
+//    {
+//        static void Main(String[] args)
+//        {
+//            int input = int.Parse(Console.ReadLine());
+//            List<int> answer = new List<int>();
 
-					answer.Sort();
-                
-            }
-            Console.WriteLine(answer);
+//            for (int i = 0; i < input; i++)
+//            {
+//                int sortNum = int.Parse(Console.ReadLine());
+//                answer.Add(sortNum);
+//            }
 
+//            answer.Sort();
 
-        }
-	}
-}
-
+//            foreach (var num in answer)
+//            {
+//                Console.WriteLine(num);
+//            }
+//        }
+//    }
+//}
