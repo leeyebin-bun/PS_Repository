6537abe9e49b516d05c577da45badc27bb388d31
yebin <na04232@naver.com>
2024-01-31 18:14:32 +0900

    0131?

---
+++ PS_Repository_CSharp/PS_Repository_CSharp/2953_ImCooker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PS_Repository_CSharp
+{
+	public class _953_ImCooker
+	{
+		static void Main(String[] args)
+		{
+			for(int i = 0; i < 5; i++)
+			{
+				string[] input = Console.ReadLine().Split(' ');
+
+			}
+		}
+	}
+}
+
